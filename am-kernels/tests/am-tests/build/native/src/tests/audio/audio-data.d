3785a87b9b5c7dbbe044a7d3ff27fc03d596e191
shikye <shikyeeee@gmail.com>
2022-12-30 21:19:28 +0800

    add dirs

---
+++ am-kernels/tests/am-tests/build/native/src/tests/audio/audio-data.d
@@ -0,0 +1,2 @@
+/home/nick/ics-pa/am-kernels/tests/am-tests/build/native/src/tests/audio/audio-data.o: \
+ /home/nick/ics-pa/am-kernels/tests/am-tests/src/tests/audio/audio-data.S
---
+++ am-kernels/tests/am-tests/build/native/src/tests/rtc.d
@@ -0,0 +1,8 @@
+/home/nick/ics-pa/am-kernels/tests/am-tests/build/native/src/tests/rtc.o: \
+ /home/nick/ics-pa/am-kernels/tests/am-tests/src/tests/rtc.c \
+ /home/nick/ics-pa/am-kernels/tests/am-tests/include/amtest.h \
+ /home/nick/ics-pa/abstract-machine/am/include/am.h \
+ /home/nick/ics-pa/abstract-machine/am/include/arch/native.h \
+ /home/nick/ics-pa/abstract-machine/am/include/amdev.h \
+ /home/nick/ics-pa/abstract-machine/klib/include/klib.h \
+ /home/nick/ics-pa/abstract-machine/klib/include/klib-macros.h
